4158b2ced99a0e3ec1bc545b2707c8e50e9eccc8
Olivia Bloway <blowayol@msu.edu>
2022-04-14 12:45:10 -0400

    touches

--- models/User.js
+++ models/User.js
@@ -4,8 +4,7 @@ const mongoose = require('mongoose')
 const UserSchema = new mongoose.Schema({
   userName: { type: String, unique: true },
   email: { type: String, unique: true },
-  budget: { type: Number, required: true},
-  currency: String,
+  budget: {type: Number},
   password: String
 })
 
